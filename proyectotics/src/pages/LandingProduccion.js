f68a912031edc49d32b5557f6418f839eba72147
MatricSous <benjamin.cofre_s@mail.udp.cl>
2024-10-28 12:17:41 -0300

    Actualizacion previa

--- proyectotics/src/pages/LandingProduccion.js
+++ proyectotics/src/pages/LandingProduccion.js
@@ -1,53 +1,63 @@
-import React from 'react';
-//import Axios from 'axios';
-import { useState } from 'react';
+// LandingProduccion.js
+
+import React, { useState } from 'react';
 import { Container, Grid, Button } from '@mui/material';
-import '../index.css';
+import RecipeModal from '../components/Modals/RecipeModal'; // Ajusta la ruta según sea necesario
 import SettingsIcon from '@mui/icons-material/Settings';
-import StoreIcon from '@mui/icons-material/Store';
-import ConstructionIcon from '@mui/icons-material/Construction';
-import LocalMallIcon from '@mui/icons-material/LocalMall';
-import PlumbingIcon from '@mui/icons-material/Plumbing';
 import CategoryIcon from '@mui/icons-material/Category';
-import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
 import InventoryIcon from '@mui/icons-material/Inventory';
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
 
 export default function LandingProduccion() {
+  const [openModal, setOpenModal] = useState(false);
 
+  const handleOpen = () => {
+    setOpenModal(true);
+  };
 
-    return (
-        <div 
-        style={{backgroundColor: '#e5e5e5' , }}>
-        
-        <Container bmaxWidth="sm" style={{ height: '100vh',paddingTop:'120px' ,display: 'flex', alignItems: 'center', justifyContent: 'center',}}>
+  const handleClose = () => {
+    setOpenModal(false);
+  };
+
+  return (
+    <div style={{ backgroundColor: '#e5e5e5' }}>
+      <Container maxWidth="sm" style={{ height: '100vh', paddingTop: '120px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <Grid container style={{ position: 'relative', height: '400px', width: '300px' }}>
+
+          {/* Botón de recetas */}
+          <RecipeModal open={openModal} handleClose={handleClose} />
           
-          {/* Botón superior */}
-          <Grid item xs={12} style={{ position: 'absolute', top: 0, left: '50%', transform: 'translateX(-50%)' }}>
-            <Button className='botonGrande'  variant="contained" startIcon={<CategoryIcon style={{ fontSize: '35px' }}/>} sx={{ width: '300px', height: '75px' , fontSize:'30px' }} color="amarillohoverblanco">Productos</Button>
-          </Grid>
-            
-          {/* Botón inferior */}
-          <Grid item xs={12} style={{ position: 'absolute', bottom: '15%', left: '50%', transform: 'translateX(-50%)' }}>
-            <Button variant="contained" color="azul" startIcon={<SettingsIcon />} >Ajustes</Button>
-          </Grid>
-  
-          {/* Botón izquierdo */}
-          <Grid item xs={12}  style={{ position: 'absolute', top: '50%', left: '-90%', transform: 'translateY(-50%)', paddingRight: '16px' }}>
-            <Button variant="contained" color="amarillohoverblanco" startIcon={<InventoryIcon style={{ fontSize: '35px' }} />} sx={{ width: '300px', height: '75px', fontSize:'30px', display: 'flex'}}
-                  >Bodegas </Button>
-          </Grid>
-  
-          {/* Botón derecho */}
           <Grid item xs={12} style={{ position: 'absolute', top: '50%', right: '-90%', transform: 'translateY(-50%)' }}>
-            <Button variant="contained" color="amarillohoverblanco" startIcon={<ReceiptLongIcon style={{ fontSize: '35px' }}/>} sx={{ width: '300px', height: '75px', fontSize:'30px'  }}>Recetas</Button>
+            <Button
+              variant="contained"
+              color="amarillohoverblanco"
+              startIcon={<ReceiptLongIcon style={{ fontSize: '35px' }} />}
+              onClick={handleOpen}
+              sx={{ width: '300px', height: '75px', fontSize: '30px' }}
+            >
+              Recetas
+            </Button>
           </Grid>
-          
+
+          <Grid item xs={12} style={{ position: 'absolute', top: 0, left: '50%', transform: 'translateX(-50%)' }}>
+                        <Button className='botonGrande' variant="contained" startIcon={<CategoryIcon style={{ fontSize: '35px' }} />} sx={{ width: '300px', height: '75px', fontSize: '30px' }} color="amarillohoverblanco">Productos</Button>
+            </Grid>
+
+          <Grid item xs={12} style={{ position: 'absolute', bottom: '15%', left: '50%', transform: 'translateX(-50%)' }}>
+            <Button variant="contained" color="azul" startIcon={<SettingsIcon />}>Ajustes</Button>
+          </Grid>
+
+          <Grid item xs={12} style={{ position: 'absolute', top: '50%', left: '-90%', transform: 'translateY(-50%)', paddingRight: '16px' }}>
+            <Button variant="contained" color="amarillohoverblanco" startIcon={<InventoryIcon style={{ fontSize: '35px' }} />} sx={{ width: '300px', height: '75px', fontSize: '30px', display: 'flex' }}>
+              Bodegas
+            </Button>
+          </Grid>
+
         </Grid>
       </Container>
-      </div>
-      );
-    
 
-
+      
+      
+    </div>
+  );
 }
